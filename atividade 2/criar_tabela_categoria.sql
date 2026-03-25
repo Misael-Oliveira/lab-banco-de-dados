6d4959680e6be317eb3c080943ca4c4f6d25b6a5
Misael Silva Oliveira <82273415+Misael-Oliveira@users.noreply.github.com>
2026-03-25 19:59:53 -0300

    Update criar_tabela_categoria.sql

--- atividade 2/criar_tabela_categoria.sql
+++ atividade 2/criar_tabela_categoria.sql
@@ -4,5 +4,5 @@ CREATE TABLE Categoria(
     nome VARCHAR(128) UNIQUE NOT NULL ,
     descricao TEXT,
     data_cadastro TIMESTAMP,
-    data_autuaizacao TIMESTAMP
+    data_autualizacao TIMESTAMP
 ); 
