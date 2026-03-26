d49d576d55c65ae629b8ac1658950a97b6a29302
Misael-Oliveira <misaelsilvaoliveira1@gmail.com>
2026-03-25 21:32:28 -0300

    Atividade 3 e comandos de DML

---
+++ DML/comandos_manipulacao.sql
@@ -0,0 +1,10 @@
+INSERT INTO livro (id_usuario, id_autor, id_editora, id_categoria, titulo, sinopse, ano_publicacao) VALUES (1, 1, 1, 1, "Ética a Nicômaco" , "Uma das mais expressivas e fecundas obras do pensamento grego e das que mais influenciaram a posteridade do mundo ocidental, Ética a Nicômaco revela o mais produtivo e laborioso filósofo da antiguidade ás voltas com a conceituação e a investigação dos elementos fundamentais da ciência do caráter e dos costumes.Trata-se de texto de referencia indispensável e obrigatório a todos os estudantes e cultivadores da filosofia, das artes e das ciências humanas, destacadamente o direito a psicologia, a antropologia, a sociologia e a política.", 2002);
+INSERT INTO Usuario (nome, email, senha) VALUES("Ryan Reynolds", "ryan.reynolds@email.com", "deadpool");
+
+INSERT INTO Categoria (nome, descricao) VALUES("Filosofia", "");
+
+INSERT INTO Autor (nome, apresentacao) VALUES("Aristótles", "");
+
+INSERT INTO Editora (nome, cidade, estado, pais) VALUES("Vozes", "São Paulo", "São Paulo", "Brasil");
+
+
---
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS biblioteca_1s_2026;
+CREATE DATABASE biblioteca_1s_2026;
+USE biblioteca_1s_2026;
+
+CREATE TABLE Usuario(
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+); 
+
+CREATE TABLE Categoria(
+	id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) UNIQUE NOT NULL ,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_autualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+); 
+
+CREATE TABLE Autor (
+	id_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (255) UNIQUE NOT NULL,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Editora(
+	id_editora INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) UNIQUE NOT NULL ,
+    cidade VARCHAR(64),
+    estado VARCHAR(64),
+    pais VARCHAR(64),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Livro(
+	id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    id_autor INT,
+    id_categoria INT,
+    id_editora INT,
+    sinopse text,
+    titulo VARCHAR(255) NOT NULL,
+    ano_publicacao YEAR,
+    lido BOOLEAN DEFAULT (0),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_livro_usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+    ON DELETE CASCADE,
+    
+    CONSTRAINT fk_livro_autor FOREIGN KEY (id_autor) REFERENCES Autor(id_autor)
+    ON DELETE RESTRICT,
+    
+    CONSTRAINT fk_livro_categoria FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria)
+    ON DELETE RESTRICT,
+    
+    CONSTRAINT fk_livro_editora FOREIGN KEY (id_editora) REFERENCES Editora(id_editora)
+    ON DELETE RESTRICT
+); 
